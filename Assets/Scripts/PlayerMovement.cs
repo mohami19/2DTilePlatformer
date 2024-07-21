741fd9b7e1b48fd28388cf395c8cc330212ce4f5
Mohammad Mahdi Gholami <gholamimahdi680@gmail.com>
2024-07-21 10:40:49 +0330

    Implement Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    Vector2 moveInput;
+    private Rigidbody2D rb;
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue inputValue){
+        moveInput = inputValue.Get<Vector2>();
+        // if (moveInput.x < 0) {
+        //     transform.localScale = new Vector3(-1,transform.localScale.y,transform.localScale.z);
+        // } else if(moveInput.x > 0){
+        //     transform.localScale = new Vector3(1,transform.localScale.y,transform.localScale.z);
+        // }
+    }
+
+    void Run(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed,rb.velocity.y);
+        rb.velocity = playerVelocity ;
+        
+    }
+
+    private void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed){
+            transform.localScale = new Vector2(Math.Sign(rb.velocity.x),1f);
+        }
+    }
+}
